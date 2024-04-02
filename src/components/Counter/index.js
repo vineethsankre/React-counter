9a138c446b28043808f3f04c33bc1a873def59bf
vineethsankre <lsvineethsankre@gmail.com>
2024-04-02 17:46:30 +0000

    Completed

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -3,14 +3,26 @@ import {Component} from 'react'
 import './index.css'
 
 class Counter extends Component {
+  state = {count: 0}
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+  onDecrement = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
   render() {
+    const {count} = this.state
     return (
       <div className="container">
         <h1 className="heading">Counter</h1>
         <p className="count">0</p>
         <div>
-          <button className="button">Increment</button>
-          <button className="button">Decrement</button>
+          <button className="button" onClick={this.onIncrement}>
+            Increment
+          </button>
+          <button className="button" onClick={this.onDecrement}>
+            Decrement
+          </button>
         </div>
       </div>
     )
